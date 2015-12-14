43029cb59c8cee5e7fe9279917f2d16520ccd323
Max Gonzih <gonzih@gmail.com>
2015-12-14 16:44:26 +0100

    Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lonely-twitch
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lonely_twitch/core_test.clj
@@ -0,0 +1,7 @@
+(ns lonely-twitch.core-test
+  (:require [clojure.test :refer :all]
+            [lonely-twitch.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
